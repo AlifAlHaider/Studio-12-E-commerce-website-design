4957dc798044b618f338dcd767ca926fdd73dfa1
AlifAlHaider <alifhaider009@gmail.com>
2023-07-25 18:54:09 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "80%";
+    
+  }
+  
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+  
+  window.onclick = function(event) {
+   
+   
+    
+    console.log('kckckckc')
+  }
+ 
+
+
